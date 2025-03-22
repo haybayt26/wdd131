3ff2e4fd28a96a7214c5b60bc8c2af4909018b6e
haybayt26 <haybayt26@gmail.com>
2025-03-21 23:31:31 -1000

    Done!

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,13 +1,17 @@
-const currentYear = document.querySelector('#currentYear');
-lastModified = document.querySelector('#lastModified');
+let year = new Date().getFullYear();
+document.getElementById("currentYear").textContent = year;
 
-const today = new Date();
-let lastModified = new Date(document.lastModified);
-full.innerHTML = `Today is <span class="highlight">${new Intl.DateTimeFormat(
-	"en-US",
-	{
-		dateStyle: "full"
-	}
-).format(today)}</span>`;
 
-currentYear.innerHTML = `getFullYear(): ${today.getFullYear()}`;
+document.addEventListener("DOMContentLoaded", function() {
+    let lastModifiedDate = new Date(document.lastModified);
+    let formattedDate = lastModifiedDate.toLocaleString('en-US', {
+        year: 'numeric', 
+        month: 'long', 
+        day: 'numeric', 
+        hour: '2-digit', 
+        minute: '2-digit',
+        second: '2-digit'
+    });
+
+    document.getElementById("lastModified").textContent = formattedDate;
+});
